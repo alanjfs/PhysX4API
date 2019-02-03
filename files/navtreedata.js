6705f437e676b32f67a29b9b69eae0c7ba8d8980
Alan Jefferson <alanjefferson.contact@gmail.com>
2019-02-03 19:30:28 +0000

    Initial commit

---
+++ files/navtreedata.js
@@ -0,0 +1,88 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PhysX 4.0 API Documentation", "index.html", [
+    [ "PhysX License", "index.html#intro_sec", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", "functions_eval" ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"GuContactBuffer_8h.html",
+"PxIO_8h.html",
+"PxWindowsIntrinsics_8h.html#a39a4b10ef013902b46ab72a6f301faaf",
+"classPxArticulationReducedCoordinate.html#aea7152a77f9446960d65359e482595c4",
+"classPxContactSet.html#a614a3d5bbdfe9fccce114e0ce64913a8",
+"classPxDeserializationContext.html#a18007433a2cc90ef8ccf4353fb8b123d",
+"classPxJoint.html",
+"classPxObstacleContext.html#a53b1408cf617fc505dc1bbf19f94f1ff",
+"classPxRigidBody.html#a8a697a7a4b9bdd2c83a68e84b9bc3a35",
+"classPxSerialization.html#a1d6242bea0c7513bfbfb24670e5cfb03",
+"classPxTriangleMesh.html#a168cc0000372c63857d128d04f1d32fa",
+"classPxVehicleDrive.html#a02bbb8b1aad17272d533b7b3a41f016f",
+"classPxVehicleGearsData.html#a31359110050aad08510b99208a304cb7a80583db4ae487006afc324f9216023d1",
+"classPxVehicleWheelsSimData.html#acbbac4e2b9c31e2955957b5868bd6e8b",
+"globals_func.html",
+"group__extensions.html#gaab84d6896f2bf7d17487799f5135cd7c",
+"group__foundation.html#ga7bd33cbf0ea64aee9a79758f70050cdf",
+"group__geomutils.html#ga9acd5763a76bcf9f41b42c3a30a6421a",
+"group__vehicle.html#ga8d4cfac998db5828bcfff2253c3d1fcc",
+"structPxConstraintFlag.html#a86960e99b3e80ddb9e0ab51d7afc3427a91151f48e43ade5a5d7153f1b3160696",
+"structPxConvexMeshCookingResult.html#ac1c7b1cbfc80b0af00bec95faf1ebf42affbc9585c9485ff2719bbfdd27df3b90",
+"structPxHitCallback.html#a9c6fc25eb66c22dad2a35d5630d4ec68",
+"structPxRigidBodyFlag.html",
+"structPxVehicleDriveTankWheelOrder.html#ad777dedf3db655580fb92de098646cb7a74a5e899e35c8aabf662a0696ae881f2"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
